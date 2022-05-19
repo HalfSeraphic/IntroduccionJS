6e9c96f4eab7bf1185e03ff2af3f8e12f9c7f681
HalfSeraphic <VIradVT@gmail.com>
2022-05-18 23:58:43 -0500

    Subida de archivos

---
+++ js/01.js
@@ -0,0 +1,19 @@
+// Variables
+/* (var es palabra reservada) (producto es nombre de la funcion)= ('Audifonos Gamer' asigna el valor); */
+var producto = 'Audifonos Gamer'; // Variable con valor
+var disponible; // Variable sin valor
+producto = true; // Reasignando el valor de la variable
+disponible = true;
+
+var producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'Computadoras'; // Con esa sintaxis se crean multiples variables
+
+// var 1disponible o -disponible no funcionan pues no pueden comenzar mas que con letras a menos que sea un guion bajo
+// Estilos para las variables
+var nombre_producto = 'Monitor HD'; //Underscore
+var nombreProducto = 'Monitor HD'; // Camelcase
+var NombreProducto = 'Monitor HD'; // Pascal Case
+var nombreproducto = 'Monitor HD'; // lower Case
+
+console.log(producto);
