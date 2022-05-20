d8634b7327dff1fe9d6e7988e5fce283d76a02a1
HalfSeraphic <VIradVT@gmail.com>
2022-05-20 13:49:54 -0500

    Continuo aprendiendo

--- js/06.js
+++ js/06.js
@@ -7,6 +7,11 @@ resultado = Math.PI;
 resultado = Math.round(2.3);
 resultado = Math.ceil(2.9); //Redondea hacia arriba
 resultado = Math.floor(2.9); //Redondea hacia abajo
+resultado = Math.sqrt(144); //Da la razin cuadrada
+resultado = Math.abs (-200); //Convierte negativo en positivo
+resultado = Math.min (3, 4 ,5, 9, 7, 29); //Obtiene el valor mas bajo
+resultado = Math.max (3, 4 ,5, 9, 7, 29); //Obtiene el valor mas alto
+resultado = Math.floor (Math.random () * 30) //Emplea varias operaciones
 
 
 console.log(resultado);
