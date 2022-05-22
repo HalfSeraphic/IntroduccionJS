22c4ecac7e8c515115e23d64834d7a5495318690
HalfSeraphic <VIradVT@gmail.com>
2022-05-22 12:04:07 -0500

    Volviendo a Linux

--- js/05.js
+++ js/05.js
@@ -1,13 +1,12 @@
 //Numeros
-const nombre = 'Victor';
-const numero1 = '100';
+const nombre = "Victor";
+const numero1 = "100";
 const numero2 = 200;
-const numero3 = 200.20;
-const numero4 = .15;
+const numero3 = 200.2;
+const numero4 = 0.15;
 const numero5 = -5;
 const numero6 = 11;
 const numero7 = 3;
-
 
 console.log(nombre);
 console.log(numero1); //Este valor volvera en negro junto al anterior pues aunque sea un numero ambos son String por estar en comillas
@@ -19,4 +18,3 @@ console.log(numero3 + numero2);
 console.log(numero3 - numero2);
 console.log(numero3 - numero2);
 console.log(numero6 % numero7); //Residuo entre dos numeros (operador modulo)
-
