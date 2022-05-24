9169b39d7065aa7fac18e3a05b4201f0509898da
HalfSeraphic <VIradVT@gmail.com>
2022-05-24 00:12:26 -0500

    Aprendiendo concatenacio y template strings

---
+++ js/07.js
@@ -0,0 +1,24 @@
+// Orden de las operaciones
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * 1.16;
+
+// console.log(resultado);
+
+// Incrementos
+
+let puntaje = 10;
+
+//Incremento en 1 ++
+puntaje++; //11
+//++puntaje; //12 
+//Para decrementos se usa el -
+
+puntaje += 10; //Especifica el incremento
+
+
+
+console.log(puntaje);
---
+++ js/08.js
@@ -0,0 +1,9 @@
+const nombre = 'Juan';
+const email = 'correo@correo.com';
+
+// Concatenacion
+/*  console.log(nombre + " " + email); //el espacio en blanco dentro de las comillas representa un espacio en blanco */
+console.log("Nombre Cliente: " + nombre + " Email: " + email) //Esta es la forma antigua
+
+//Template Strings - String literals
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`); //Es la forma nueva
